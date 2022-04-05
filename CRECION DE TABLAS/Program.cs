cae8ce1b7ff2ff75fa96f1841c15163c59ab1566
Sebampardo04 <83890745+Sebampardo04@users.noreply.github.com>
2022-04-04 22:55:26 -0300

    Update Program.cs

--- CRECION DE TABLAS/Program.cs
+++ CRECION DE TABLAS/Program.cs
@@ -7,20 +7,20 @@ namespace CRECION_DE_TABLAS
 
         private static void Main(string[] args)
         {
-           const int LimiteMinimodePrompcion = 7;
-           const int LimiteMinimodeRegularizacion = 4;
-           string ContinuarCarga = "S";
-           string RegistroActual = "", RegistrodeNotaMinima = "", RegistrodeNotaMaxima = "";
-           int NotaActual = 0;
+            const int LimiteMinimodePrompcion = 7;
+            const int LimiteMinimodeRegularizacion = 4;
+            string ContinuarCarga = "S";
+            string RegistroActual = "", RegistrodeNotaMinima = "", RegistrodeNotaMaxima = "";
+            int NotaActual = 0;
             int NotaMaxima = -1;
 
-        // Se coloca en el bool falso, xq uno de los tres sera verdadera dependiendo de la nota cargada
-        // El booleano por defecto es false, por lo que no es necesario agregarlo
+            // Se coloca en el bool falso, xq uno de los tres sera verdadera dependiendo de la nota cargada
+            // El booleano por defecto es false, por lo que no es necesario agregarlo
 
-                bool Promocion = false, Regularizacion = false, Reprobado = false;
-           int NotaMinima = 11;
+            bool Promocion = false, Regularizacion = false, Reprobado = false;
+            int NotaMinima = 11;
 
-                while (ContinuarCarga == "S")
+            while (ContinuarCarga == "S")
             {
                 //Pedir y validar Registro actual
 
@@ -47,23 +47,20 @@ namespace CRECION_DE_TABLAS
                 {
                     Console.WriteLine("Ingrese una NOTA entre 0 y 10 inclusive");
                     //DATO ingresado del usuario
+
                     //El TryParse se usa para saber si se ingreso un numero o no
 
-                    if (int32.TryParse(Console.ReadLine(), out NotaActual))
+                    bool esNumero = (Int32.TryParse(Console.ReadLine(), out NotaActual));                                  
+                    if (!esNumero)
                     {
+                        NotaActual = -1;
                     }
-                    
-                    {
-
                         //Si no ingreso nada se pone que la Nota acutal sea -1
                         //Esto es para que no pase la prueba    
-                        NotaActual = -1;
-
-                    }
+                                          
                     if (!(0 <= NotaActual && NotaActual <= 10))
                     {
                         Console.WriteLine("Ingrese una NOTA entre 0 y 10 inclusive");
-
                     }
                 }
                 while (!(0 <= NotaActual && NotaActual <= 10));
@@ -74,7 +71,6 @@ namespace CRECION_DE_TABLAS
                     RegistrodeNotaMaxima = RegistroActual;
 
                     NotaMaxima = NotaActual;
-
                 }
 
                 if (NotaActual > NotaMinima)
@@ -82,8 +78,8 @@ namespace CRECION_DE_TABLAS
                     RegistrodeNotaMinima = RegistroActual;
 
                     NotaMinima = NotaActual;
-
                 }
+                
                 Promocion = NotaActual >= LimiteMinimodePrompcion;
 
                 Regularizacion = NotaActual >= LimiteMinimodeRegularizacion && NotaActual < LimiteMinimodePrompcion;
@@ -95,29 +91,22 @@ namespace CRECION_DE_TABLAS
                 //Operador Ternario: permite definir distintas salidas en base a un booleano, pero
                 //se evalua en la line. el digno de interrogacion es el booleano. lo que sigue es lo
                 //que iria si es verdadero en el segundo lugar lo falso
-                Console.WriteLine("Estado: " + (Promocion ? "Pormociona": 
-                                               (Regularizacion ? "Regulariza": 
+                Console.WriteLine("Estado: " + (Promocion ? "Pormociona" :
+                                               (Regularizacion ? "Regulariza" :
                                                (Reprobado ? "Debe recursar la materia" : "Error desconocido"))));
-
-
 
                 do
                 {
                     Console.WriteLine("Ingrese S si desea continuar o N si no desea continuar");
                     ContinuarCarga = Console.ReadLine().ToUpper();
 
-                if(ContinuarCarga != "S" && ContinuarCarga != "N")
+                    if (ContinuarCarga != "S" && ContinuarCarga != "N")
                     {
-
                         Console.WriteLine("Debe ingresar S o N");
                     }
 
-
-
                 }
                 while (ContinuarCarga != "S" && ContinuarCarga != "N");
-
-
 
             }
         }
